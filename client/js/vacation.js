bfb4bec7fb18caae1b3a75c0c17dc0cd90f20d76
SDR9973 <shiradr95@gmail.com>
2024-09-23 04:54:20 +0300

    added fetchVacations in server

--- client/js/vacation.js
+++ client/js/vacation.js
@@ -1,12 +1,7 @@
-const url = 'HTTP://LOCALHOST:5001';
+const url = 'http://localhost:5001';
 
 window.onload = async () => {
     try {
-        pageCheck();
-        const currencyData = await getCurrency();
-        const currentDate = getDateTime();
-        const categoriesData = await getCategories();
-        loadList(currencyData, currentDate, categoriesData);
         eventListeners();
     }
     catch (error) {
@@ -14,74 +9,14 @@ window.onload = async () => {
     }
 };
 
-const cookies = document.cookie.split('; ').reduce((acc, cookie) => {
-    const [name, value] = cookie.split('=');
-    acc[name] = value;
-    return acc;
-}, {});
-
-pageCheck = () => {
-    const childId = cookies['accountId'];
-    console.log(childId);
-    if (!childId) {
-        console.error('Child ID not found in cookies');
-        throw new Error('Child ID not found in cookies');
-    }
-};
-
-getCurrency = async () => {
-    const childId = cookies['accountId'];
-    try {
-        const response = await fetch(`${url}/api/wallets/${childId}`);
-        const data = await response.json();
-        return data.currency;
-    }
-    catch (error) {
-        throw new Error('Error fetching currencies:', error);
-    }
-};
-
-getDateTime = () => {
-    const today = Date.now() - (new Date().getTimezoneOffset() * 60000);
-    const ISOformat = new Date(today).toISOString();
-    return ISOformat.slice(0, 16);
-};
-
-getCategories = async () => {
-    try {
-        const response = await fetch(`data/categories.json`);
-        const data = await response.json();
-        return data.categories;
-    }
-    catch (error) {
-        throw new Error('Error fetching categories:', error);
-    }
-}
-
-loadList = (currencyData, currentDate, categoriesData) => {
-    const currencyElement = document.getElementById('currency');
-    currencyElement.innerHTML = currencyData;
-
-    const dateElement = document.getElementById('date');
-    dateElement.value = currentDate;
-
-    const categoryElement = document.getElementById('category');
-    categoriesData.forEach(category => {
-        const optionElement = document.createElement('option');
-        optionElement.value = category.title;
-        optionElement.innerHTML = category.title;
-        categoryElement.appendChild(optionElement);
-    });
-};
-
 eventListeners = () => {
-    document.getElementById('newExpenseForm').addEventListener(`submit`, (event) => {
+    document.getElementById('newVacationForm').addEventListener(`submit`, (event) => {
         event.preventDefault();
-        submitExpense(event);
+        submitVacation(event);
     });
 };
 
-submitExpense = async (event) => {
+submitVacation = async (event) => {
     const formData = new FormData(event.target);
     const name = formData.get('name');
     const price = Number(formData.get('price'));
@@ -105,6 +40,6 @@ submitExpense = async (event) => {
         console.log(data)
     }
     catch (error) {
-        console.error('Error submitting expense:', error);
+        console.error('Error submitting vacation:', error);
     }
 };
